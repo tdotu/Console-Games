33dd686be51939e923c0c1dc96b3f207f2e28170
BlitzBasic <phoesch@gmx.de>
2013-12-19 14:52:12 +0100

    commit new hangman version (not useable yet)

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -1,17 +1,167 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "hangman.h"
 
 
 
+wordList *words = NULL;
+
+
+
 int main(int argc, char **argv)
 {
-    char solution[] = "university";
+    initRandomGenerator();
 
-    askWord(solution, 10);
+    words = loadWordFile("test.hmn");
+
+    mainMenue();
 
     return 0;
+}
+
+
+void mainMenue()
+{
+    int option = 0;
+
+    do
+    {
+        option = printMenue();
+
+        switch(option)
+        {
+            case 1:
+                mainMenueOption1();
+            break;
+
+            case 2:
+                mainMenueOption2();
+            break;
+
+            case 3:
+                mainMenueOption3();
+            break;
+
+            case 4:
+                mainMenueOption4();
+            break;
+
+            case 5:
+                mainMenueOption5();
+            break;
+
+            case 6:
+                mainMenueOption6();
+            break;
+
+            case 7:
+                mainMenueOption7();
+            break;
+
+            default:
+                mainMenueOptionDefault();
+            break;
+        }
+    }while(option != 7);
+}
+
+
+int printMenue()
+{
+    int choice = 0;
+
+    printf("\n");
+    printf("Main menu\n");
+    printf("---------\n");
+    printf("\n");
+    printf("1. Play word guessing (random word)\n");
+    printf("2. Play word guessing (self entered word)\n");
+    printf("3. Load a word library\n");
+    printf("4. Enter a word by hand\n");
+    printf("5. Save the loaded words to a new word library\n");
+    printf("5. Unload all words\n");
+    printf("6. Show the credits\n");
+    printf("7. Exit this program\n");
+    printf("\n");
+    printf("Your selection: < >\b\b");
+    scanf("%d", &choice);
+    printf("\n");
+
+    return choice;
+}
+
+
+void mainMenueOption1()
+{
+    if(words == NULL)
+    {
+        printf("Something went wrong (the word list is not existing)\n");
+        return;
+    }
+
+    if(words->numOfWords == 0)
+    {
+        printf("You have to put words in the word list before you can play!\n");
+        return;
+    }
+
+    if(askWord(getRandomWord(words), STANDART_NUMBER_OF_GUESSES) == TRUE)
+    {
+        printf("Gratulations!\n");
+        printf("You found the word!\n");
+    }
+    else
+    {
+        printf("Don't worry.\n");
+        printf("Maybe you'll win next time.\n");
+    }
+}
+
+
+void mainMenueOption2()
+{
+    printf("This option is not implemented yet.\n");
+}
+
+
+void mainMenueOption3()
+{
+    printf("This option is not implemented yet.\n");
+}
+
+
+void mainMenueOption4()
+{
+    printf("This option is not implemented yet.\n");
+}
+
+
+void mainMenueOption5()
+{
+    printf("Unloading all words...\n");
+    deleteAllElements(words);
+    printf("All words unloaded.\n");
+}
+
+
+void mainMenueOption6()
+{
+    printf("This option is not implemented yet.\n");
+}
+
+
+void mainMenueOption7()
+{
+    printf("Program will be ended...\n");
+}
+
+
+void mainMenueOptionDefault()
+{
+    printf("Your choosen selection does not exist.\n");
 }
 
 
@@ -40,10 +190,18 @@ truth askWord(char *word, int maxGuesses)
         if(guessCorrect == NO)
             tryCounter++;
         else if(guessCorrect == YES && checkSolved(word, currentWord) == TRUE)
+        {
+            printf("\n");
+            printf("Solved word: %s\n", currentWord);
+            printf("\n");
             return TRUE;
+        }
+
 
         printf("\n");
     }
+
+    printf("Solution: %s\n\n", word);
 
     return FALSE;
 }
@@ -54,9 +212,11 @@ guessedCorrectly askCharacter(char *finalWord, char *currentWord, char *triedCha
     char guess = '\0';
 
     printf("Next guess: ");
-    fflush(stdin);
-    guess = getchar();
-    getchar();              // clears the input buffer (may not work on all computers)
+
+    do
+    {
+        guess = getchar();
+    }while(guess == '\0' || guess == '\n');
 
     if(checkCharacter(guess, triedCharacters))
     {
@@ -118,4 +278,161 @@ void addCharacter(char character, char *charlist)
     int listsize = strlen(charlist);
     charlist[listsize] = character;
     charlist[++listsize] = '\0';
+}
+
+
+char *appendWordList(wordList *wl, char *newEntry)
+{
+    wordListEntry *listIterator = wl->first;
+
+    if(newEntry == NULL)
+        return;
+
+    while(listIterator->next != NULL)
+        listIterator = listIterator->next;
+
+    listIterator->next = (wordListEntry *)malloc(sizeof(wordListEntry));
+    listIterator = listIterator->next;
+    listIterator->next = NULL;
+    listIterator->word = newEntry;
+    wl->numOfWords++;
+
+    return newEntry;
+}
+
+
+void deleteAllElements(wordList *wl)
+{
+    while(wl->numOfWords > 0)
+        deleteLastElement(wl);
+}
+
+
+void deleteLastElement(wordList *wl)
+{
+    wordListEntry *listIterator = wl->first;
+
+    while(listIterator->next->next != NULL)
+        listIterator = listIterator->next;
+
+    free(listIterator->next);
+    listIterator->next = NULL;
+    wl->numOfWords--;
+}
+
+
+wordList *createWordList()
+{
+    wordList *newList = (wordList *)malloc(sizeof(wordList));
+    newList->first = (wordListEntry *)malloc(sizeof(wordListEntry));    // create a dummy
+    newList->first->next = NULL;
+    newList->first->word = NULL;
+    newList->numOfWords = 0;
+
+    return newList;
+}
+
+
+char *getElement(wordList *wl, int element)
+{
+    int i = 0;
+    wordListEntry *listIterator = wl->first;
+
+    for(i = 0; i < element; i++)
+    {
+        if(listIterator->next != NULL)
+            listIterator = listIterator->next;
+        else
+        {
+            printf("unable to get element %d from list %p\n", element, wl);
+            return NULL;
+        }
+    }
+
+    return listIterator->word;
+}
+
+
+char *readLine(FILE *stream)
+{
+    long lineBegin = ftell(stream);
+    char *line = NULL;
+
+    char currentChar = '\0';
+    int charCounter = 0;
+
+    if(feof(stream))
+        return NULL;
+
+    for(charCounter = 0; currentChar != '\n' && currentChar != '\0'; charCounter++)
+        currentChar = getc(stream);
+
+    line = (char *)malloc((sizeof(char) * (charCounter + 1)));
+
+    fseek(stream, SEEK_SET, lineBegin);
+    fgets(line, MAX_WORDLENGTH, stream);
+    line[charCounter-1] = '\0';
+
+    for(charCounter = 0; line[charCounter] != '\0'; charCounter++)
+        if(line[charCounter] == '\n')
+            line[charCounter] = '\0';
+
+    return line;
+}
+
+
+wordList *loadWordFile(char *filename)
+{
+    wordList *newList = createWordList();
+    return addWordFile(filename, newList);
+}
+
+
+wordList *addWordFile(char *filename, wordList *wl)
+{
+    FILE *f = fopen(filename, "r");
+
+    if(f == NULL)
+    {
+        printf("unable to read file %s\n", filename);
+        return NULL;
+    }
+
+    while(!feof(f))
+        if(*appendWordList(wl, readLine(f)) == '\0')
+            deleteLastElement(wl);
+
+    deleteLastElement(wl);
+
+    fclose(f);
+
+    return wl;
+}
+
+
+void initRandomGenerator()
+{
+    srand(time(0));
+}
+
+
+char *getRandomWord(wordList *wl)
+{
+    int number = rand() % wl->numOfWords+1;
+
+    return getElement(wl, number);
+}
+
+
+int printWordList(wordList *wl)
+{
+    wordListEntry *listIterator = wl->first->next;
+
+    while(listIterator != NULL)
+    {
+        printf("%s\n", listIterator->word);
+        listIterator = listIterator->next;
+    }
+
+    return wl->numOfWords;
 }
--- hangman/hangman.h
+++ hangman/hangman.h
@@ -4,7 +4,9 @@
 
 
 #define MAX_WORDLENGTH 30
-#define LETTERS_IN_ALPHABET 32
+#define LETTERS_IN_ALPHABET 62
+
+#define STANDART_NUMBER_OF_GUESSES 10
 
 
 enum truth
@@ -18,9 +20,35 @@ enum guessedCorrectly
 };
 
 
+struct wordListEntry
+{
+    char *word;
+    struct wordListEntry *next;
+};
+
+struct wordList
+{
+    int numOfWords;
+    struct wordListEntry *first;
+};
+
+
 typedef enum truth truth;
 typedef enum guessedCorrectly guessedCorrectly;
+typedef struct wordListEntry wordListEntry;
+typedef struct wordList wordList;
 
+
+void mainMenue();
+int printMenue();
+void mainMenueOption1();
+void mainMenueOption2();
+void mainMenueOption3();
+void mainMenueOption4();
+void mainMenueOption5();
+void mainMenueOption6();
+void mainMenueOption7();
+void mainMenueOptionDefault();
 
 truth askWord(char *word, int maxGuesses);
 guessedCorrectly askCharacter(char *finalWord, char *currentWord, char *triedCharacters);
@@ -28,6 +56,20 @@ truth checkSolved(char *finalWord, char *currentWord);
 truth checkCharacter(char character, char *word);
 void uncoverCharacters(char character, char *finalWord, char *currentWord);
 void addCharacter(char character, char *charlist);
+
+char *appendWordList(wordList *wl, char *newEntry);
+void deleteAllElements(wordList *wl);
+void deleteLastElement(wordList *wl);
+wordList *createWordList();
+char *getElement(wordList *wl, int element);
+char *readLine(FILE *stream);
+wordList *loadWordFile(char *filename);
+wordList *addWordFile(char *filename, wordList *wl);
+
+void initRandomGenerator();
+char *getRandomWord(wordList *wl);
+
+int printWordList(wordList *wl);        // debug
 
 
 
